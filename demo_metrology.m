317632188f685d25c110662e548a7ea4dc01c85d
Veerendra C Angadi <vcangadi1@users.noreply.github.com>
2017-09-15 19:32:29 +0100

    Variance of Cov[X] is calculated

--- demo_metrology.m
+++ demo_metrology.m
@@ -34,7 +34,7 @@ A(:,:,10) = I_r;
 
 X = reshape(A, r*c, N);
 
-[COEFF,SCORE,LATENT] = pca(X);
+[COEFF,SCORE,LATENT] = pca(zscore(X));
 
 %% Plot Eigen values of Covariance matrix
 
@@ -43,3 +43,18 @@ xlabel('Principle Components (PC)');
 ylabel('Var\{PC\} or Eig\{Cov[X]\}');
 grid on
 grid minor
+
+%% Calculate PC1 and PC2
+
+PC1 = zeros(size(COEFF));
+PC1(:,1) = COEFF(:,1);
+PC1 = reshape((SCORE*PC1'), r, c, N);
+Ipc1 = PC1(:,:,1);
+
+PC2 = zeros(size(COEFF));
+PC2(:,2) = COEFF(:,2);
+PC2 = reshape((SCORE*PC2'), r, c, N);
+Ipc2 = PC2(:,:,2);
+
+
+reshape(Itransformed(:,1),size(I,1),size(I,2));
