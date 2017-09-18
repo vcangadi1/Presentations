b5b988a2523ff0fa38ccda638624ef9eab15da4f
Veerendra C Angadi <vcangadi1@users.noreply.github.com>
2017-09-18 16:59:10 +0100

    PC1 and PC2 are observed in biplot

--- demo_metrology.m
+++ demo_metrology.m
@@ -1,60 +1,71 @@
 clc;
+close all;
+clear all;
 
 %% Define constants
 
-% Number of samples
-N = 20;
-% Rotation angle
-rt = 5;
-
-% Image size
-r = 50;
-c = 50;
+N  = 100; % Number of samples
+rt = 45; % Rotation angle
+r  = 50; % Image size (rows)
+c  = 50; % Image size (columns)
 
 % Image noise parameters
-M = 0; %Mean
-V = 5; %Variance
+M  = 0; %Mean
+V  = 1; %Variance
 
-%% Create test images
+%% Create test reference image
 
-I = zeros(r,c);
-I(15:end-15,15:end-15) = 1;
-% Add gaussian noise
-I = imnoise(I,'gaussian',0,5); % Reference
+I0 = zeros(r,c);
+I0(15:end-15,20:end-20) = 1; I = I0;
+I = imnoise(I0,'gaussian', M, V); % Add gaussian noise
 
-I_r = imrotate(I, rt, 'bilinear', 'crop');
-% Add gaussian noise
-I_r = imnoise(I_r,'gaussian',0,5); % 
+%% Create fault image by rotation
+I_r = imrotate(I0, rt, 'bilinear', 'crop'); % Rotate image
+I_r = imnoise(I_r,'gaussian', M, V); % Add gaussian noise
 
+
+%% Cascade to create batch
 A = repmat(I,1,1,N);
+A(:,:,round(N/2)) = I_r;
 
-A(:,:,10) = I_r;
+%I_r = imrotate(I0, 5, 'bilinear', 'crop'); % Rotate image
+%I_r = imnoise(I_r,'gaussian', M, V); % Add gaussian noise
+%A(:,:,N) = I_r;
+
+%I_r = imrotate(I0, 10, 'bilinear', 'crop'); % Rotate image
+%I_r = imnoise(I_r,'gaussian', M, V); % Add gaussian noise
+%A(:,:,N-10) = I_r;
+
+figure;
+subplot 131
+imshow(I0)
+subplot 132
+imshow(I)
+subplot 133
+imshow(I_r)
 
 %% Apply PCA
 
 X = reshape(A, r*c, N);
 
-[COEFF,SCORE,LATENT] = pca(zscore(X));
+[COEFF,SCORE,LATENT] = pca(X);
 
 %% Plot Eigen values of Covariance matrix
 
+figure;
 loglog(LATENT, '*-', 'LineWidth', 2)
 xlabel('Principle Components (PC)');
 ylabel('Var\{PC\} or Eig\{Cov[X]\}');
 grid on
 grid minor
 
+figure;
+%bar(LATENT./sum(LATENT))
+pie(LATENT./sum(LATENT))
+
 %% Calculate PC1 and PC2
 
-PC1 = zeros(size(COEFF));
-PC1(:,1) = COEFF(:,1);
-PC1 = reshape((SCORE*PC1'), r, c, N);
-Ipc1 = PC1(:,:,1);
+figure;
+vbls = cellstr(string(1:N));
+biplot(COEFF(:,1:2),'Scores',SCORE(:,1:2),'VarLabels',vbls)
 
-PC2 = zeros(size(COEFF));
-PC2(:,2) = COEFF(:,2);
-PC2 = reshape((SCORE*PC2'), r, c, N);
-Ipc2 = PC2(:,:,2);
-
-
-reshape(Itransformed(:,1),size(I,1),size(I,2));
