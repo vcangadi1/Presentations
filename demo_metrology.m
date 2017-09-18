1e1edc2e58ecb92071691be67d74dfc3ab16a8a6
Veerendra C Angadi <vcangadi1@users.noreply.github.com>
2017-09-18 18:32:52 +0100

    Random orientation faults detected

--- demo_metrology.m
+++ demo_metrology.m
@@ -3,55 +3,43 @@ close all;
 clear all;
 
 %% Define constants
-
-N  = 100; % Number of samples
-rt = 45; % Rotation angle
+N  = 100; % Batch size
 r  = 50; % Image size (rows)
 c  = 50; % Image size (columns)
 
 % Image noise parameters
 M  = 0; %Mean
-V  = 1; %Variance
+V  = 2; %Variance
 
-%% Create test reference image
+% Create random orientations and positions
+num_rt = randi([1 5]); % Number of faulty orientation
+rt = randi([1 360],1,num_rt); % Rotation angles
+Pos= randi([1 N],1,length(rt)); % Frame location faulty orientation
 
+% Create test reference image
 I0 = zeros(r,c);
-I0(15:end-15,20:end-20) = 1; I = I0;
+I0(15:end-15,20:end-20) = 100; I = I0;
 I = imnoise(I0,'gaussian', M, V); % Add gaussian noise
 
-%% Create fault image by rotation
-I_r = imrotate(I0, rt, 'bilinear', 'crop'); % Rotate image
-I_r = imnoise(I_r,'gaussian', M, V); % Add gaussian noise
-
-
-%% Cascade to create batch
+% Cascade to create batch
 A = repmat(I,1,1,N);
-A(:,:,round(N/2)) = I_r;
 
-%I_r = imrotate(I0, 5, 'bilinear', 'crop'); % Rotate image
-%I_r = imnoise(I_r,'gaussian', M, V); % Add gaussian noise
-%A(:,:,N) = I_r;
-
-%I_r = imrotate(I0, 10, 'bilinear', 'crop'); % Rotate image
-%I_r = imnoise(I_r,'gaussian', M, V); % Add gaussian noise
-%A(:,:,N-10) = I_r;
-
+% Create fault image by rotation
 figure;
-subplot 131
-imshow(I0)
-subplot 132
-imshow(I)
-subplot 133
-imshow(I_r)
+for ii = 1:length(rt)
+    I_r = imrotate(I0, rt(ii), 'bilinear', 'crop'); % Rotate image
+    I_r = imnoise(I_r,'gaussian', M, V); % Add gaussian noise
+    A(:,:,Pos(ii)) = I_r;
+    subplot(length(rt),1,ii)
+    imshow(A(:,:,Pos(ii)))
+    title({['Orientation = ',num2str(rt(ii)),'\circ'];['Frame = ', num2str(Pos(ii))]})
+end
 
 %% Apply PCA
-
 X = reshape(A, r*c, N);
-
 [COEFF,SCORE,LATENT] = pca(X);
 
-%% Plot Eigen values of Covariance matrix
-
+% Plot Eigen values of Covariance matrix
 figure;
 loglog(LATENT, '*-', 'LineWidth', 2)
 xlabel('Principle Components (PC)');
@@ -59,13 +47,10 @@ ylabel('Var\{PC\} or Eig\{Cov[X]\}');
 grid on
 grid minor
 
-figure;
-%bar(LATENT./sum(LATENT))
-pie(LATENT./sum(LATENT))
+%figure;
+%pie(LATENT./sum(LATENT))
 
-%% Calculate PC1 and PC2
-
+% Calculate PC1 and PC2
 figure;
 vbls = cellstr(string(1:N));
 biplot(COEFF(:,1:2),'Scores',SCORE(:,1:2),'VarLabels',vbls)
-
